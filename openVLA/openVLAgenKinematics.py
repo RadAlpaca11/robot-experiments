1d3ba31f21b4c971dc5c9f052011d2747da79cd7
Dan Rosenstein <danrose_microsoft>
2025-03-25 17:08:49 -0700

    add debug launch file and remove flash attentionfrom getting started

--- openVLA/openVLAgenKinematics.py
+++ openVLA/openVLAgenKinematics.py
@@ -15,7 +15,7 @@ import torch
 processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
 vla = AutoModelForVision2Seq.from_pretrained(
     "openvla/openvla-7b", 
-    attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
+    # attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
     torch_dtype=torch.bfloat16, 
     low_cpu_mem_usage=True, 
     trust_remote_code=True
